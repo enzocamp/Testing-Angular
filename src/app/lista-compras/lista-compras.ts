93a7abdcab5adb89c5c6b78365cfe4c345097e6c
Enzo Campolongo <enzo.campolongo@mktechsolutions.com.br>
2025-09-12 19:37:53 -0300

    Adding logic to increase shopping list

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.html',
   styleUrl: './lista-compras.scss'
 })
 export class ListaCompras {
+  novoItem: string = '';
+  itens: { id: number, nome: string, comprado: boolean }[] = [
+    { id: 1, nome: 'Arroz', comprado: false },
+    { id: 2, nome: 'Macarrão', comprado: false }
+  ];
 
+  adicionarItem() {
+    if (this.novoItem.trim()) {
+      const novoId = this.itens.length > 0 ? Math.max(...this.itens.map(item => item.id)) + 1 : 1;
+      console.log(this.novoItem);
+      this.itens.push({
+        id: novoId,
+        nome: this.novoItem.trim(),
+        comprado: false
+      });
+      this.novoItem = '';
+    }
+  }
 }
