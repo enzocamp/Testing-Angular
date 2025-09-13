d63b84db583bd5da98ac2f7cb962f0d7bcabf73c
Enzo Campolongo <enzo.campolongo@mktechsolutions.com.br>
2025-09-13 19:09:48 -0300

    Including model to itemlist

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
 
 @Component({
   selector: 'app-lista-compras',
@@ -9,21 +10,19 @@ import { FormsModule } from '@angular/forms';
 })
 export class ListaCompras {
   novoItem: string = '';
-  itens: { id: number, nome: string, comprado: boolean }[] = [
-    { id: 1, nome: 'Arroz', comprado: false },
-    { id: 2, nome: 'Macarrão', comprado: false }
-  ];
+  lista: ItemLista[] = [];
+  
 
   adicionarItem() {
-    if (this.novoItem.trim()) {
-      const novoId = this.itens.length > 0 ? Math.max(...this.itens.map(item => item.id)) + 1 : 1;
-      console.log(this.novoItem);
-      this.itens.push({
-        id: novoId,
-        nome: this.novoItem.trim(),
-        comprado: false
-      });
-      this.novoItem = '';
-    }
+    let itemLista = new ItemLista();
+    itemLista.nome = this.novoItem;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
+    
+    this.novoItem = '';
+
+    console.table(this.lista);
   }
 }
