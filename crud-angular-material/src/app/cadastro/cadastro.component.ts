2d3260fa5fa3677b4727d33ade38be7677b29009
Enzo Campolongo <enzo.campolongo@mktechsolutions.com.br>
2025-09-15 18:33:11 -0300

    Capturing data from form to use on service

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-cadastro',
@@ -15,4 +16,9 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class CadastroComponent {
 
+    cliente: Cliente = Cliente.newCliente();
+
+    salvar(){
+      console.log(this.cliente);
+    }
 }
