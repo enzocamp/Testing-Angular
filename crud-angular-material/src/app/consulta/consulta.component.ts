ab9380dbaf44377ad4356a1764fa19e673b61409
Enzo Campolongo <enzo.campolongo@mktechsolutions.com.br>
2025-09-15 23:54:46 -0300

    Using Table from Material

--- crud-angular-material/src/app/consulta/consulta.component.ts
+++ crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -6,13 +7,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { ClienteService } from '../cliente.service';
+import { Cliente } from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
-  imports: [MatButtonModule, MatInputModule, MatCardModule, MatTableModule, MatIconModule, FlexLayoutModule, FormsModule],
+  imports: [MatButtonModule, MatInputModule, MatCardModule, MatTableModule, MatIconModule, FlexLayoutModule, FormsModule, CommonModule],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit{
 
+  listaClientes: Cliente[] = [];
+  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email"];
+  constructor(private service: ClienteService ){
+
+  }
+
+  ngOnInit(){
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
 }
