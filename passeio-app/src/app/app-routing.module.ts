0605898c116d02f89171bceab7dbe8e6abbd90df
enzocamp <enzo.flap@hotmail.com>
2025-10-28 22:52:30 -0300

    configurando rotas do template.module no app.routing.module

--- passeio-app/src/app/app-routing.module.ts
+++ passeio-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'paginas', // quando acessa /paginas, sera acessado o template module e o que tiver nela
+    loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)//aqui ele carrega tudo que declararmos em nosso module
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
